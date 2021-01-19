664499dbc92d863f411ca5f1c8f03c826f4074a2
Emanuel Dobra <ema.dobra@gmail.com>
2021-01-19 11:17:56 -0500

    Add pre-final A1 edit

--- Assignments/Assignment1.cpp
+++ Assignments/Assignment1.cpp
@@ -27,7 +27,13 @@ int main() {
     bool extraBed, enterGuest = true;
     double totalCharge;
     std::string roomTypeOutput; 
+    // Open output file
     std::ofstream fout("charges.txt");
+    if (!fout.is_open()) {
+        std::cout << "error opening file";
+        system("pause");
+        exit(-1);
+    }
     // Write title to file
     fout << std::fixed << std::setprecision(2);
     fout << std::left << std::setw(40) << "CPA Resort Hotel Room Charge\n"
@@ -75,7 +81,16 @@ int main() {
  * @return the valid room type
 */
 char getValidRoomType() {
-
+    char roomType; 
+    std::cout << "Enter the guest's room type (G/P/L): ";
+    std::cin >> roomType;
+    do {
+        std::cin.clear();
+        std::cin.ignore(80, '\n');
+        std::cout << "Wrong input, enter G P or L: ";
+        std::cin >> roomType;
+    } while ((roomType != 'G' && roomType != 'P' && roomType != 'L') || std::cin.fail());
+    return roomType;
 }
 
 /**
@@ -84,7 +99,21 @@ char getValidRoomType() {
  * @return the room rate 
 */
 double determineRate(char roomType) {
-
+    double roomRate;
+    switch(roomType) {
+        case 'G':
+            roomRate = 125.00;
+            break;
+        case 'P':
+            roomRate = 145.00;
+            break;
+        case 'L':
+            roomRate = 180.00;
+            break;
+        default:
+            std::cout << "Undefined room type";
+    }
+    return roomRate;
 }
 
 /**
@@ -92,21 +121,56 @@ double determineRate(char roomType) {
  * @return the amount of days guest stayed
 */
 int getValidDays() {
-
+    int daysAmount;
+    std::cout << "Enter the amount of days the guest stayed: ";
+    std::cin >> daysAmount;
+    do {
+        std::cin.clear();
+        std::cin.ignore(80, '\n');
+        std::cout << "Wrong input, enter a number between 0 and 31: ";
+        std::cin >> daysAmount;
+    } while (daysAmount < 0 || daysAmount > 31 || std::cin.fail());
+    return daysAmount;
 }
 
 /**
  * Ask for the user's answer to a yes/no question
- * @param response message to show the user
+ * @param message message to show the user
  * @return the user's answer as a boolean
 */
 bool validateReply(std::string message) {
-    
+    std::string answer;
+    std::cout << message;
+    std::getline(std::cin, answer);
+    do {
+        std::cin.clear();
+        std::cin.ignore(80,'\n');
+        std::cout << "Inavlid input, enter yes or no: ";
+        std::getline(std::cin, answer);
+    } while ((answer != "yes" && answer != "no") || std::cin.fail());
+    if (answer == "yes") 
+        return true;
+    else 
+        return false;
 }
 
 /**
- * Compute the 
+ * Compute the total room charge of the guest
+ * @param roomType the room type of the guest
+ * @param restDays total days guest stayed
+ * @param extraBed whether or not the guest wanted an extra bed
 */
 double computeRoomCharge(char roomType, int restDays, bool extraBed) {
-
+    double roomRate;
+    roomRate = determineRate(roomType);
+    if (extraBed) {
+        switch (roomType) {
+            case 'L':
+                roomRate += 20;
+                break;
+            default:
+                roomRate += 15;
+        }
+    }
+    return (roomRate * restDays);
 }

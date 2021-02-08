b9ebf26d5effac97ea33758f640ce1139a48166d
Emanuel Dobra <ema.dobra@gmail.com>
2021-02-08 13:48:37 -0500

    Updated A1

--- Assignments/Assignment1.cpp
+++ Assignments/Assignment1.cpp
@@ -4,11 +4,11 @@
 #include <iomanip>
 
 /**
- * Due Feb19 2021. 
+ * Due Feb19 2021.
 */
 
 /***************************************************************************
-Emanuel Dobra   
+Emanuel Dobra
 Assignment 1
 Generates a report file for hotel guests.
 ***************************************************************************/
@@ -26,7 +26,8 @@ int main() {
     int daysAmount;
     bool extraBed, enterGuest;
     double roomCharges, totalCharges = 0;
-    std::string roomTypeOutput; 
+    std::string roomTypeOutput;
+
     // Open output file
     std::ofstream fout("charges.txt");
     if (!fout.is_open()) {
@@ -34,31 +35,32 @@ int main() {
         system("pause");
         exit(-1);
     }
+
     // Write title to file
     fout << std::fixed << std::setprecision(2);
-    fout << std::left << std::setw(40) << "CPA Resort Hotel Room Charge\n"
-        << std::left << std::setw(50) << "****************\n"
-        << std::right << std::setw(15) << "Guest Name" 
-        << std::right << std::setw(15) << "Room Type"
-        << std::left << std::setw(20) << "Room Charge\n\n";
-    do {
+    fout << std::right << std::setw(40) << "CPA Resort Hotel Room Charge\n"
+        << std::right << std::setw(36) << "********************\n"
+        << std::left << std::setw(20) << "Guest Name"
+        << std::left << std::setw(20) << "Room Type"
+        << std::right << std::setw(20) << "Room Charge\n\n";
+    do {        
         std::cout << "Enter the guest's name: ";
-        std::cin >> guestName; 
+        getline(std::cin, guestName);
         // Get room type
         roomType = getValidRoomType();
         // Set room type output
-        switch(roomType) {
-            case 'G':
-                roomTypeOutput = "Garden View";
-                break;
-            case 'P':
-                roomTypeOutput = "Pool View";
-                break;
-            case 'L':
-                roomTypeOutput = "Lake View";
-                break;
-            default:
-                std::cout << "Unknown room type\n"; 
+        switch (roomType) {
+        case 'G':
+            roomTypeOutput = "Garden View";
+            break;
+        case 'P':
+            roomTypeOutput = "Pool View";
+            break;
+        case 'L':
+            roomTypeOutput = "Lake View";
+            break;
+        default:
+            std::cout << "Unknown room type\n";
         }
         // Get total days
         daysAmount = getValidDays();
@@ -68,15 +70,16 @@ int main() {
         roomCharges = computeRoomCharge(roomType, daysAmount, extraBed);
         totalCharges += roomCharges;
         // Output entry to file
-        fout << std::right << std::setw(15) << guestName 
-            << std::right << std::setw(15) << roomTypeOutput
-            << std::left << std::setw(20) << roomCharges << std::endl;
+        fout << std::left << std::setw(20) << guestName
+            << std::left << std::setw(20) << roomTypeOutput
+            << std::right << std::setw(18) << roomCharges << std::endl;
         // Ask for next entry
         enterGuest = validateReply("Do you want to enter another guest? Answer yes/no: ");
+        std::cin.ignore(80, '\n');
     } while (enterGuest);
-
-    fout << std::right << std::setw(30) << "Total Charges: " 
-        << std::left << std::setw(20) << totalCharges << std::endl;
+    std::cout << "\n";
+    fout << std::left << std::setw(40) << "Total Charges: "
+        << std::right << std::setw(18) << totalCharges << std::endl;
 
     system("type charges.txt");
     system("pause");
@@ -87,7 +90,7 @@ int main() {
  * @return the valid room type
 */
 char getValidRoomType() {
-    char roomType; 
+    char roomType;
     std::cout << "Enter the guest's room type (G/P/L): ";
     std::cin >> roomType;
     roomType = toupper(roomType);
@@ -97,29 +100,29 @@ char getValidRoomType() {
         std::cout << "Wrong input, enter G P or L: ";
         std::cin >> roomType;
         roomType = toupper(roomType);
-    } 
+    }
     return roomType;
 }
 
 /**
  * Get the room rate based on the room type
  * @param roomType the type of room
- * @return the room rate 
+ * @return the room rate
 */
 double determineRate(char roomType) {
     double roomRate;
-    switch(roomType) {
-        case 'G':
-            roomRate = 125.00;
-            break;
-        case 'P':
-            roomRate = 145.00;
-            break;
-        case 'L':
-            roomRate = 180.00;
-            break;
-        default:
-            std::cout << "Undefined room type";
+    switch (roomType) {
+    case 'G':
+        roomRate = 125.00;
+        break;
+    case 'P':
+        roomRate = 145.00;
+        break;
+    case 'L':
+        roomRate = 180.00;
+        break;
+    default:
+        std::cout << "Undefined room type";
     }
     return roomRate;
 }
@@ -132,12 +135,12 @@ int getValidDays() {
     int daysAmount;
     std::cout << "Enter the amount of days the guest stayed: ";
     std::cin >> daysAmount;
-    while (daysAmount < 0 || daysAmount > 31 || std::cin.fail()) {
+    while (daysAmount < 1 || daysAmount > 31 || std::cin.fail()) {
         std::cin.clear();
         std::cin.ignore(80, '\n');
-        std::cout << "Wrong input, enter a number between 0 and 31: ";
+        std::cout << "Wrong input, enter a number between 1 and 31: ";
         std::cin >> daysAmount;
-    } 
+    }
     return daysAmount;
 }
 
@@ -149,16 +152,16 @@ int getValidDays() {
 bool validateReply(std::string message) {
     std::string answer;
     std::cout << message;
-    std::getline(std::cin, answer);
+    std::cin >> answer;
     while ((answer != "yes" && answer != "no") || std::cin.fail()) {
         std::cin.clear();
-        std::cin.ignore(80,'\n');
+        std::cin.ignore(80, '\n');
         std::cout << "Inavlid input, enter yes or no: ";
         std::getline(std::cin, answer);
     }
-    if (answer == "yes") 
+    if (answer == "yes")
         return true;
-    else 
+    else
         return false;
 }
 
@@ -167,18 +170,18 @@ bool validateReply(std::string message) {
  * @param roomType the room type of the guest
  * @param restDays total days guest stayed
  * @param extraBed whether or not the guest wanted an extra bed
- * @return 
+ * @return
 */
 double computeRoomCharge(char roomType, int restDays, bool extraBed) {
     double roomRate;
     roomRate = determineRate(roomType);
     if (extraBed) {
         switch (roomType) {
-            case 'L':
-                roomRate += 20;
-                break;
-            default:
-                roomRate += 15;
+        case 'L':
+            roomRate += 20;
+            break;
+        default:
+            roomRate += 15;
         }
     }
     return (roomRate * restDays);
